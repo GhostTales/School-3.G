573cafeaf11fdb21a2863932a60d0c033e31b5a1
GhostTales <danjuulchristensen1@gmail.com>
2023-08-15 11:27:09 +0200

    deleted non school programs

--- Programs/1. & 2. G/bbcode python/RunDisplay.vbs
+++
@@ -1,3 +0,0 @@
-Set WshShell = CreateObject("WScript.Shell") 
-WshShell.Run chr(34) & "htmlDisplay.exe" & Chr(34), 0
-Set WshShell = Nothing
--- Programs/1. & 2. G/stream apps opener python/stream apps.py
+++
@@ -1,7 +0,0 @@
-import os
-import webbrowser
-
-with open('file_paths.txt') as f:
-    for file_path_or_url in f:
-        file_path_or_url = file_path_or_url.strip().replace('\\', r'\\')
-        (webbrowser.open if file_path_or_url.startswith('http') else os.startfile)(file_path_or_url)
