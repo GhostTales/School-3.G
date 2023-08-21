d6f820626db19eca2bc0bd44ba0617aee06b12f6
GhostTales <danjuulchristensen1@gmail.com>
2023-08-21 09:52:52 +0200

    made a program to check if a word is a palindrom

---
+++ Programs/3.G/Palindrom/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+bool IsPalindrome(string s)
+{
+    if (s.Length <= 1)
+        return true;
+
+    if (s[0] == s[s.Length - 1])
+        return IsPalindrome(s.Substring(1, s.Length - 2));
+
+    return false;
+}
+
+string word = "atom";
+
+if (IsPalindrome(word) == true)
+    Console.WriteLine(word + " is a palindrome");
+else
+    Console.WriteLine(word + " is not a palindrome");
+
