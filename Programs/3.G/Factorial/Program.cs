a3b467aab05dee3cb9857e8feaee398be98212e1
GhostTales <danjuulchristensen1@gmail.com>
2023-08-21 10:21:01 +0200

    made program to calculate factorials

---
+++ Programs/3.G/Factorial/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int Fakultet(int i)
+{
+    if (i < 0)
+        return 0;
+    if (i == 0)
+        return 1;
+    if (i == 1)
+        return i;
+
+    return i * Fakultet(i - 1);
+}
+
+Console.WriteLine(Fakultet(32));
