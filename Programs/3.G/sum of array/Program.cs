3c248b688199e127da646baa0654a1752d20690c
GhostTales <danjuulchristensen1@gmail.com>
2023-08-26 13:05:13 +0200

    assignments of sum in an array

--- Programs/3.G/sum of array/Program.cs
+++ Programs/3.G/sum of array/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+int[] nummbers = { 1, 2, 3, 4, 5 };
+
+int Summen(int[] arr, int i)
+{
+
+    if (i <= 1)
+        return arr[0];
+
+    return Summen(arr, i - 1) + arr[i - 1];
+
+
+
+}
+
+Console.WriteLine(Summen(nummbers, nummbers.Length));
