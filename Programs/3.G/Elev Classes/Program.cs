07a4c4050ffb9900bd487193a00f6e05a39625ad
GhostTales <danjuulchristensen1@gmail.com>
2023-08-28 14:34:55 +0200

    made stuff

---
+++ Programs/3.G/Elev Classes/Program.cs
@@ -0,0 +1,59 @@
+﻿
+using System;
+using Microsoft.VisualBasic;
+
+Elev[] elever = new Elev[100];
+
+for (int i = 0; i < elever.Length; i++)
+{
+    elever[i] = new Elev();
+}
+
+Elev minElev = new Elev();
+Elev minElev2 = new Elev();
+Elev minElev3 = new Elev();
+Elev minElev4 = new Elev();
+
+
+minElev.SetNavn("Dan");
+minElev.SetAlder(19);
+minElev.SetSnit(7.8f);
+
+minElev2.SetAll("Thomas", 48, 7.7f);
+minElev3.SetAll("Søren", 18, 7.6f);
+minElev4.SetAll("Jens", 20, 7.9f);
+
+
+minElev.ElevInfo();
+minElev2.ElevInfo();
+minElev3.ElevInfo();
+minElev4.ElevInfo();
+
+public class Elev
+{
+    private int alder;
+    private string navn = "";
+    private float snit;
+
+    public void SetAlder(int n) { alder = n; }
+    public int GetAlder() { return alder; }
+    public void SetNavn(string s) { navn = s; }
+    public string GetNavn() { return navn; }
+    public void SetSnit(float f) { snit = f; }
+    public float GetSnit() { return snit; }
+
+    public void ElevInfo()
+    {
+        Console.WriteLine("Alder: " + this.GetAlder());
+        Console.WriteLine("Navn: " + this.GetNavn());
+        Console.WriteLine("Gennemsnit " + this.GetSnit());
+        Console.WriteLine("----------------------------");
+    }
+
+    public void SetAll(string s, int n, float f)
+    {
+        this.SetNavn(s);
+        this.SetAlder(n);
+        this.SetSnit(f);
+    }
+}
---
+++ Programs/3.G/Terning Class/Program.cs
@@ -0,0 +1,49 @@
+﻿
+using System.Globalization;
+using System;
+using System.Security.Cryptography;
+
+Dice die = new Dice();
+
+Console.WriteLine("Throw: " + die.Throw());
+Console.WriteLine("Throw: " + die.Throw());
+Console.WriteLine("prev Value: " + die.PrevValue());
+Console.WriteLine("Throw: " + die.Throw());
+die.ThrowMany(10);
+Console.WriteLine("many Throws: " + die.GetValueMany());
+
+
+public class Dice
+{
+    private int value;
+
+    private int[] throwMany;
+    private Random rnd = new Random();
+    private string array = "";
+
+    public int Throw()
+    {
+        return value = rnd.Next(1, 6);
+    }
+
+    public int[] ThrowMany(int n)
+    {
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.Length; i++)
+        {
+            arr[i] = rnd.Next(1, 6);
+        }
+        throwMany = arr;
+        return arr;
+    }
+    public string GetValueMany()
+    {
+        foreach (int i in throwMany)
+        {
+            array += (i + " ");
+        }
+        return array;
+    }
+    public int PrevValue() { return value; }
+
+}
