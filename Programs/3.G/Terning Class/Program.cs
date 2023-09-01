4bafab6069c737c685addb6082be8633bced20cc
GhostTales <danjuulchristensen1@gmail.com>
2023-09-01 13:55:37 +0200

    Terning class was to complex made it simple

--- Programs/3.G/Terning Class/Program.cs
+++ Programs/3.G/Terning Class/Program.cs
@@ -1,49 +1,31 @@
 ﻿
-using System.Globalization;
-using System;
-using System.Security.Cryptography;
+
 
 Dice die = new Dice();
+Dice die2 = new Dice();
 
-Console.WriteLine("Throw: " + die.Throw());
-Console.WriteLine("Throw: " + die.Throw());
-Console.WriteLine("prev Value: " + die.PrevValue());
-Console.WriteLine("Throw: " + die.Throw());
-die.ThrowMany(10);
-Console.WriteLine("many Throws: " + die.GetValueMany());
+int sum = 0;
+int sum2 = 0;
+for (int i = 0; i < 10; i++)
+{
+    sum += die.Throw(6);
+    Console.WriteLine("Die PrevValue: " + die.PrevValue());
+}
 
+for (int i = 0; i < 10; i++)
+{
+    sum2 += die2.Throw(6);
+    Console.WriteLine("Die2 PrevValue: " + die2.PrevValue());
+}
+
+Console.WriteLine("sum fo die: " + sum);
+Console.WriteLine("sum fo die2: " + sum2);
 
 public class Dice
 {
     private int value;
-
-    private int[] throwMany;
     private Random rnd = new Random();
-    private string array = "";
 
-    public int Throw()
-    {
-        return value = rnd.Next(1, 6);
-    }
-
-    public int[] ThrowMany(int n)
-    {
-        int[] arr = new int[n];
-        for (int i = 0; i < arr.Length; i++)
-        {
-            arr[i] = rnd.Next(1, 6);
-        }
-        throwMany = arr;
-        return arr;
-    }
-    public string GetValueMany()
-    {
-        foreach (int i in throwMany)
-        {
-            array += (i + " ");
-        }
-        return array;
-    }
+    public int Throw(int n) { return value = rnd.Next(1, n); }
     public int PrevValue() { return value; }
-
 }
