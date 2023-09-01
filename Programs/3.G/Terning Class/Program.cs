6a5eed4796b62a7d84fd9751a652c713f2bd8828
GhostTales <danjuulchristensen1@gmail.com>
2023-09-01 14:58:17 +0200

    die got finished

--- Programs/3.G/Terning Class/Program.cs
+++ Programs/3.G/Terning Class/Program.cs
@@ -1,9 +1,20 @@
 ﻿
 
+Dice die4 = new Dice(4);
+Dice die6 = new Dice(6);
+Dice die8 = new Dice(8);
+Dice die10 = new Dice(10);
+Dice die12 = new Dice(12);
 Dice die20 = new Dice(20);
 
+Console.WriteLine($"die4: {die4.Throw()}");
+Console.WriteLine($"die6: {die6.Throw()}");
+Console.WriteLine($"die8: {die8.Throw()}");
+Console.WriteLine($"die10: {die10.Throw()}");
+Console.WriteLine($"die12: {die12.Throw()}");
 Console.WriteLine($"die20: {die20.Throw()}");
 
+Console.WriteLine(die20.info());
 
 public class Dice
 {
@@ -18,6 +29,7 @@ public class Dice
     }
 
     public int Throw() { return value = rnd.Next(1, max + 1); }
+    public string info() { return $"previous value: {value} | max value of die: {max}"; }
     public int PrevValue() { return value; }
     public int MaxValue() { return max; }
 }
