76b260f3ef0d14064346e31672963c86973e6003
GhostTales <danjuulchristensen1@gmail.com>
2023-09-01 14:41:35 +0200

    made terning better by storing the max inside itself

--- Programs/3.G/Terning Class/Program.cs
+++ Programs/3.G/Terning Class/Program.cs
@@ -9,13 +9,13 @@ int sum2 = 0;
 for (int i = 0; i < 10; i++)
 {
     sum += die.Throw(6);
-    Console.WriteLine("Die PrevValue: " + die.PrevValue());
+    Console.WriteLine($"Die PrevValue: {die.PrevValue()} | max: {die.MaxValue()}");
 }
 
 for (int i = 0; i < 10; i++)
 {
-    sum2 += die2.Throw(6);
-    Console.WriteLine("Die2 PrevValue: " + die2.PrevValue());
+    sum2 += die2.Throw(8);
+    Console.WriteLine($"Die2 PrevValue: {die2.PrevValue()} | max: {die2.MaxValue()}");
 }
 
 Console.WriteLine("sum fo die: " + sum);
@@ -25,7 +25,9 @@ public class Dice
 {
     private int value;
     private Random rnd = new Random();
+    private int max;
 
-    public int Throw(int n) { return value = rnd.Next(1, n); }
+    public int Throw(int n) { max = n; return value = rnd.Next(1, n); }
     public int PrevValue() { return value; }
+    public int MaxValue() { return max; }
 }
