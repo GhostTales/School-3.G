9330fc498ca595f8fd8b8a697f92331dc2f1e597
GhostTales <danjuulchristensen1@gmail.com>
2023-09-11 20:10:06 +0200

    godot fucking sucks

--- Programs/3.G/ball bounce/Label.cs
+++ Programs/3.G/ball bounce/Label.cs
@@ -1,27 +1,21 @@
 using Godot;
 using System;
 
-public partial class bat : CharacterBody2D
+public partial class scorelabel : Label
 {
-
+	
+	private int _score = 10;
+	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+	
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		float AMOUNT = 10;
-		if (Input.IsKeyPressed(Key.A))
-		{
-			MoveAndCollide(new Vector2(-AMOUNT,0));
-			
-		}
-		if (Input.IsKeyPressed(Key.D))
-		{
-			MoveAndCollide(new Vector2(AMOUNT,0));
-		}
+		
+		this.Text = $"score: {_score}";
 	}
-
 }
